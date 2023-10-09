099bc41e7833d887f7a68fb783ef736d3505116e
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-10-09 15:18:52 +0200

    add contactSlise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,45 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Form from './Form/Form';
-import { nanoid } from 'nanoid';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser, deleteUser } from 'redux/contactSlice';
+import { filterSearch } from 'redux/filterSlise';
+import { nanoid } from '@reduxjs/toolkit';
+import { selectContacts, selectFilter } from 'redux/selector';
 
 export default function App() {
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts')) ?? []
-  );
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const dispatch = useDispatch();
 
-  const createUser = data => {
-    const contact = {
-      ...data,
-      id: nanoid(),
-    };
+  const handleCreateUser = data => {
     if (contacts.find(el => el.name === data.name)) {
       alert(data.name + ' is already in contacts.');
       return;
     }
-    setContacts([...contacts, contact]);
+    dispatch(createUser({ ...data, id: nanoid(5) }));
   };
+
+  const handleDeleteUser = id => dispatch(deleteUser(id));
+
   const getVisibleName = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(el =>
       el.name.toLowerCase().includes(filter.toLowerCase(normalizedFilter))
     );
   };
-  const deleteUser = id => {
-    setContacts(contacts.filter(el => el.id !== id));
-  };
-  const handleSearch = e => {
-    setFilter(e.target.value);
-  };
+
+  const handleSearch = e => dispatch(filterSearch(e.target.value));
 
   return (
     <>
       <h1>Phonebook</h1>
-      <Form createUser={createUser} />
+      <Form createUser={handleCreateUser} />
       <h2>Contacts</h2>
       <Filter filter={filter} handleSearch={handleSearch} />
-      <ContactList contacts={getVisibleName()} deleteUser={deleteUser} />
+      <ContactList contacts={getVisibleName()} deleteUser={handleDeleteUser} />
     </>
   );
 }
